db586e432d5f0e1843edf29e2b99beb36d3be6e1
Sneakoz <79712322+Sneakoz@users.noreply.github.com>
2024-03-01 02:19:03 +0500

    add size change and clear funtionality

--- script.js
+++ script.js
@@ -1,23 +1,49 @@
 const areaSize = 700;
 let pixelsOneWay = 16;
 let pixelsTotal = pixelsOneWay * pixelsOneWay;
-let pixelSize = (areaSize / pixelsOneWay) ;
-
+let pixelSize = areaSize / pixelsOneWay ;
 
 const sketchArea = document.querySelector('#sketchArea');
-sketchArea.style.width = `${areaSize}px`;
-sketchArea.style.height = `${areaSize}px`;
+sketchArea.style.width = sketchArea.style.height = `${areaSize}px`;
 
 function changePixelColor() {
     this.style.backgroundColor = 'black'
+}
+
+const sizebtn = document.querySelector('#prompt');
+sizebtn.addEventListener('click', changePixelCount);
+
+const clearbtn = document.querySelector('#clear');
+clearbtn.addEventListener('click', () => {
+    removePixels()
+    createPixels()
+})
+
+function changePixelCount() {
+    let newPixelCount = prompt('Enter Pixel Count. Max = 100');
+    console.log(newPixelCount)
+    if (newPixelCount >= 100) {
+        alert('Max Pixel count is 100 by 100')
+    } else {
+        pixelsTotal = newPixelCount * newPixelCount;
+        pixelSize = areaSize / newPixelCount
+        removePixels()
+        createPixels()
+    }
+}
+
+
+function removePixels() {
+    while (sketchArea.firstChild) {
+        sketchArea.removeChild(sketchArea.firstChild)
+    }
 }
 
 function createPixels() {
     for (let i = 0; i < pixelsTotal; i++) {
         const pixel = document.createElement('div');
 
-        pixel.style.width = `${pixelSize}px`;
-        pixel.style.height = `${pixelSize}px`;
+        pixel.style.width = pixel.style.height = `${pixelSize}px`;
         pixel.classList.add('pixel');
 
         sketchArea.appendChild(pixel);
