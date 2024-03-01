acc7c0e0181740850a7ad3c245ccfbba11f1299f
Sneakoz <79712322+Sneakoz@users.noreply.github.com>
2024-03-02 01:21:13 +0500

    style slider

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
+// Declare areaSize which is the width of the sketch area and pixelsOneWay
 const areaSize = 700;
 let pixelsOneWay = 16;
 
-
-
+// Import sketch area and set width equal to *areaSize*
 const sketchArea = document.querySelector('#sketchArea');
 sketchArea.style.width = sketchArea.style.height = `${areaSize}px`;
+
+// Import the sliderContainer ,slider and sliderValue
+const sliderContainer = document.querySelector('#sliderContainer');
+const slider = document.querySelector("#slider");
+const sliderValue = document.querySelector('#sliderValue');
+
+// Set the sliderValue to the value of the pixelsOneWay
+sliderValue.textContent = `${slider.value} x ${slider.value} Resolution`
+
 
 const sizebtn = document.querySelector('#prompt');
 sizebtn.addEventListener('click', changePixelCount);
